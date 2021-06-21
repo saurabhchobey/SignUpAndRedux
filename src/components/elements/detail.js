2295e6ba55c13f03e39553ebabcd6f572079ca80
saurabhchobey <52131942+saurabhchobey@users.noreply.github.com>
2021-06-21 06:56:08 +0530

    Add files via upload

---
+++ src/components/elements/detail.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const detail = () => {
+    return (
+       <>
+        <div>
+               DETAILS
+           </div>
+       <div className='card'>
+          
+           <table>
+       <th>Name</th>
+            <th>Phone</th>
+            <th>E-mail</th>
+            <th>Actions</th>
+            </table>
+       </div>
+       </>
+    )
+}
+
+export default detail
